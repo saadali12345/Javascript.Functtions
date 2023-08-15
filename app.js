c120f6626a58d8835758597bf6951c108e90be9f
saadali12345 <saad.ali.bscs@gmail.com>
2023-08-16 00:12:52 +0500

    Javascript Function

---
+++ app.js
@@ -0,0 +1,89 @@
+// Question Node:1
+
+
+// function calculateRectangleArea(TwoHundred,Onehundred) {
+//     return TwoHundred * Onehundred;
+// }
+
+// const TwoHundred1= 10;
+// const Onehundred1= 108;
+// const calculate = calculateRectangleArea(TwoHundred1,Onehundred1);
+// console.log(`calculate: ${calculate}`);
+
+// function calculateRectangleArea(TwoHundred,Onehundred) {
+//     return TwoHundred + Onehundred;
+// }
+
+// const TwoHundred1= 10;
+// const Onehundred1= 10;
+// const calculate = calculateRectangleArea(TwoHundred1,Onehundred1);
+// console.log(`calculate: ${calculate}`);
+
+// function calculateRectangleArea(TwoHundred,Onehundred) {
+//     return TwoHundred - Onehundred;
+// }
+
+// const TwoHundred1= 41;
+// const Onehundred1= 10;
+// const calculate = calculateRectangleArea(TwoHundred1,Onehundred1);
+// console.log(`calculate: ${calculate}`);
+
+
+
+// Question No:2
+// function greet(name,lastname) {
+//     console.log('Hello ' + name +'  ' + lastname);
+// }
+
+// greet('Saad','Ali');
+
+// Question No:3
+
+// function addTwoNumbers() {
+//     const num1 = parseFloat(prompt("Enter the first number:"));
+//     const num2 = parseFloat(prompt("Enter the second number:"));
+
+//     if (!isNaN(num1) && !isNaN(num2)) {
+//         const sum = num1 + num2;
+//         return sum;
+//     } else {
+//         return "Sahi value put karo.";
+//     }
+// }
+
+// const result = addTwoNumbers();
+// console.log(`2 number ko milanai sai yae  banega : ${result}`);
+
+
+// Question No: 4
+// function istekhara(name, governmentJob, salery, car, house, bankBalance) {
+//     if (governmentJob === true && salery >= 50000 && car === "koi bhi" && house === true && bankBalance >= 1000000) {
+//         return ("istekhara mai haan hai " + name + "larka bohot acha hai ghar walo ne dusri behan kai liye bhi haan karidya .")
+//     }
+//     else {
+//         return ("Istekhara sahi nahi aya.");
+//     }
+// }
+
+// const result = istekhara("Saad " , true, 100000, "koi bhi", true, 12300000);
+// console.log(result);
+
+
+// function istekhara(name, governmentJob, salery, car, house, bankBalance) {
+//     if (governmentJob === true && salery >= 50000 && car === "koi bhi" && house === false && bankBalance >= 1000000) {
+//         return ("istekhara mai haan hai " + name + "larka bohot acha hai ghar walo ne dusri behan kai liye bhi haan karidya .")
+//     }
+//     else {
+//         return ("larka acha hai lekin ghar apna nahi hai .");
+//     }
+// }
+
+// const result = istekhara("Saad " , true, 100000, "koi bhi", true, 12300000);
+// console.log(result);
+
+
+
+
+
+
+
